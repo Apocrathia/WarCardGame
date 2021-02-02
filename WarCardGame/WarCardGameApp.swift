cae68ebb9d9dcb89c086b9e1b533872426869a5a
Ian <ian@apocrathia.com>
2021-02-01 18:21:05 -0700

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Ian Young on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
